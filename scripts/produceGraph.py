51dadb78544c7683356b526a0854e6dd515588c8
dhakim <dhakimdahakim@gmail.com>
2019-03-08 16:47:48 -0800

    Replaced global alignment followed by hamming distance with Levenshtein distance with a max dist break out

--- scripts/produceGraph.py
+++ scripts/produceGraph.py
@@ -1,108 +1,12 @@
 #input HF.txt, SF.txt
 #output something.graph
 
+import levenshtein
 import networkx as nx
 
-def hammingDistance(p, q):
-    dist = 0
-    # loop through each char and see if they match
-    for i in range(len(p)):
-        if(p[i] != q[i]):
-            dist += 1
-            
-    return dist
-
 # finds the edit distance between two strings
-def editDist(v, w):
-    distance = 0
-    backtrack, s = editAlign(v, w)
-    newV, newW = outputEdit(backtrack, v, w)
-    distance = hammingDistance(newV, newW)
-    
-    
-    
-    return distance
-
-
-# global alignment for edit distance
-def editAlign(v, w):
-    
-    # find the dimensions of the graph
-    n = len(v)
-    m = len(w)
-    
-    sigma = 1
-    
-    # initialize the backtrack matrix to be the correct size
-    backtrack = list()
-    s = list()
-    # set up the grid for n+1 x m+1
-    for i in range(0,n+1):
-        backtrack.append(list())
-        s.append(list())
-        for j in range(0,m+1):
-            backtrack[i].append(0)
-            s[i].append(0)
-    
-    # set the first column to zeros.
-    s[0][0] = 0
-    for i in range(1,len(v)+1):
-        s[i][0] = s[i-1][0]-sigma
-        backtrack[i][0] = "|"
-    
-    # set the first row to zeros
-    for j in range(1,len(w)+1):
-        s[0][j] = s[0][j-1]-sigma
-        backtrack[0][j] = "-"
-        
-    # go through each other element and determine the weight value (0 for an insertion or deletion, 1 for a match)
-    for i in range(1,len(v)+1):
-        for j in range(1,len(w)+1):
-            
-            if v[i-1] == w[j-1]:
-                # the tile is the max of the predecessors plus the respective edges
-                s[i][j] = max([s[i-1][j]-sigma, s[i][j-1]-sigma, s[i-1][j-1]])
-            else:
-                s[i][j] = max([s[i-1][j]-sigma, s[i][j-1]-sigma, s[i-1][j-1]-1])
-            
-            # figure out which path was taken and make backtrack have the correct symbols.
-            if s[i][j]+sigma == s[i-1][j]:
-                backtrack[i][j] = "|"
-            elif s[i][j]+sigma == s[i][j-1]:
-                backtrack[i][j] = "-"
-            elif (s[i][j] == s[i-1][j-1] or s[i][j] == s[i-1][j-1]-1):
-                backtrack[i][j] = "*"
-                
-    return backtrack, s[i][j]
-
-
-def outputEdit(backtrack, v, w):
-    
-    i = len(v)
-    j = len(w)
-    alignedV = ""
-    alignedW = ""
-    
-    # go through the matrix and navigate to the souce via longest path
-    for idx in range(len(v)+len(w),0, -1):
-        
-        if backtrack[i][j] == "|":
-            i = i-1
-            alignedV += v[i]
-            alignedW += "-"
-        elif backtrack[i][j] == "-":
-            j = j-1
-            alignedV += "-"
-            alignedW += w[j]
-        elif backtrack[i][j] == "*":
-            i = i-1
-            j = j-1
-            alignedV += v[i]
-            alignedW += w[j]
-        if (i == 0 and j == 0):
-            break
-            
-    return alignedV[::-1], alignedW[::-1]
+def editDistLevenshtein(v, w, maxDist):
+    return levenshtein.compute(v,w,maxDist)
 
 # parses the data into a list.
 def readData(filename):
@@ -136,7 +40,7 @@ def createGraph(sequences, K):
 
             # otherwise determine if it's pairwise distance is less than K
             else:
-                dist = editDist(seq, pair)
+                dist = editDistLevenshtein(seq, pair, K)
                 if dist < K:
                     seqGraph.add_edge(seq, pair, weight=dist)
                 else:
